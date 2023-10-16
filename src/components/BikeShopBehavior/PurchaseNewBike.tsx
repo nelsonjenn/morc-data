34dab63b60b30548e6aad32fa8e0803120f48770
Jennifer Nelson <jennifer_nelson@mckinsey.com>
2023-10-16 14:06:26 -0500

    Graph Add Last Service

--- src/components/BikeShopBehavior/PurchaseNewBike.tsx
+++ src/components/BikeShopBehavior/PurchaseNewBike.tsx
@@ -1,97 +1,91 @@
-
 import { Box, Button, useTheme } from '@mui/material';
 import {
-    ResponsiveContainer,
-    Tooltip,
-    CartesianGrid,
-    Bar,
-    Legend,
-    XAxis,
-    YAxis,
-    ComposedChart,
-
+	ResponsiveContainer,
+	Tooltip,
+	CartesianGrid,
+	Bar,
+	Legend,
+	XAxis,
+	YAxis,
+	ComposedChart,
 } from 'recharts';
 import { useEffect, useState } from 'react';
-import purchaseData from '../data/newBike.json'
-
-
+import purchaseData from '../data/newBike.json';
 
 export type PurchaseData = {
-    answer: string;
-    value: number;
-}
-
+	answer: string;
+	value: number;
+};
 
 export default function PurchaseNewBike() {
-    
-    const [display, setDisplay] = useState(false);
-    const [data, setData] = useState<PurchaseData[]>([]);
-    const theme = useTheme();
-    
-    let graphData: PurchaseData[] = [];
-    let outstateGraphData: PurchaseData[] = [];
+	const [display, setDisplay] = useState(false);
+	const [data, setData] = useState<PurchaseData[]>([]);
+	const theme = useTheme();
 
-    
-    const handleClick = () => {
-        setDisplay(!display);
-       
-    };
+	let graphData: PurchaseData[] = [];
+	let outstateGraphData: PurchaseData[] = [];
 
-    useEffect(() => {
-        graphData = purchaseData as unknown as PurchaseData[];
-        setData(graphData);
-    },[]);
-    
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            
-            width: '100%',
-            
-        }}>
-        <Button onClick={handleClick}>When did you last get a new bike?</Button>
-        {display && 
-            <>
-                <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    width: '100%',
-                    height: '500px'
-                }}>
-                   
-                <ResponsiveContainer
-                width='100%'
-                height='100%'>
-                <ComposedChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 5,
-                    bottom: 15,
-                }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="answer" />
-                <YAxis />
-                <Bar dataKey="value" label="Last New Bike" fill={theme.palette.primary.main} />
-                
-                <Tooltip />
-               
+	const handleClick = () => {
+		setDisplay(!display);
+	};
 
-                </ComposedChart>
-               
-                
-                </ResponsiveContainer>
-               
-                </Box>
-                </>
-            }
-            </Box>
-            );
-        }
-        
+	useEffect(() => {
+		graphData = purchaseData as unknown as PurchaseData[];
+		setData(graphData);
+	}, []);
+
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+
+				width: '100%',
+			}}
+		>
+			<Button onClick={handleClick}>
+				When did you last get a new bike?
+			</Button>
+			{display && (
+				<>
+					<Box
+						sx={{
+							display: 'flex',
+							flexDirection: 'column',
+							width: '100%',
+							height: '500px',
+						}}
+					>
+						<ResponsiveContainer
+							width='100%'
+							height='100%'
+						>
+							<ComposedChart
+								width={500}
+								height={300}
+								data={data}
+								margin={{
+									top: 5,
+									right: 30,
+									left: 5,
+									bottom: 15,
+								}}
+							>
+								<CartesianGrid strokeDasharray='3 3' />
+								<XAxis dataKey='answer' />
+								<YAxis />
+								<Bar
+									dataKey='value'
+									label='Last New Bike'
+									fill={theme.palette.primary.main}
+								/>
+
+								<Tooltip />
+							</ComposedChart>
+						</ResponsiveContainer>
+					</Box>
+				</>
+			)}
+		</Box>
+	);
+}
